bd88a482086b33b02a7e934f60604749dbd80a12
HenriqueGM <henrique.g.michelini@gmail.com>
2025-01-29 14:42:51 -0300

    register command

--- craftalism_market/src/main/java/io/github/HenriqueMichelini/craftalism_market/CraftalismMarket.java
+++ craftalism_market/src/main/java/io/github/HenriqueMichelini/craftalism_market/CraftalismMarket.java
@@ -1,17 +1,22 @@
 package io.github.HenriqueMichelini.craftalism_market;
 
+import io.github.HenriqueMichelini.craftalism_market.command.MarketCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class CraftalismMarket extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("CraftalismMarket Enabled!");
+        Objects.requireNonNull(getCommand("market")).setExecutor(new MarketCommand());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("CraftalismMarket Disabled!");
     }
 }
